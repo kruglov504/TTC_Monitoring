4d353e7b0a1ea20596416371a4ca0b145c086d67
AK <alexakrugloff@gmail.com>
2023-08-20 17:29:48 +0200

    TTC data reading works

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from requests_html import HTMLSession
+#from requests_html import HTMLSession
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from os import environ, pathsep
+
+#C:\Program Files\Google\Chrome\Application\chrome.exe
+
+environ["PATH"] += pathsep + 'C:/Program Files/Google/Chrome/Application'
+browser = webdriver.Chrome()
+
+url = 'https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?IconName=crafting_outfitter_potion_014.png&ItemID=211&ItemNamePattern=Dreugh+Wax&SortBy=LastSeen&Order=desc'
+
+browser.get(url)
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+table = soup.find('table', class_ = 'trade-list-table max-width')
 
 
+for elem in table.find_all('tbody'):
+    rows = elem.find_all('tr')
+    for row in rows:
+        rowData = row.find('td', class_ = 'bold hidden-xs')
+        print(row.text)
 
-s = HTMLSession()
-
-querry = 'munich'
-url = f'https://www.google.com/search?q=weather+{querry}'
-
-r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'})
-
-
-temp = r.html.find('span#wob_tm', first=True).text
-
-print(temp)
 
 
